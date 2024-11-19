a37be663e2c0e0d8f31b3ebe4baa6ca7aae58f15
gustavobizon <gustavobizonj@gmail.com>
2024-11-18 21:09:58 -0300

    V3

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const authenticateJWT = (req, res, next) => {
             if (err) {
                 return res.status(403).json({ message: 'Acesso negado' });
             }
-            req.user = user;
+            req.user = user; 
             next();
         });
     } else {
@@ -30,7 +30,7 @@ const authenticateJWT = (req, res, next) => {
     }
 };
 
-// Middleware de validação de dados dos sensores
+// Middleware de validação de dados
 const validateSensorData = (req, res, next) => {
     const dadosArray = Array.isArray(req.body) ? req.body : [req.body];
 
@@ -49,19 +49,18 @@ const validateSensorData = (req, res, next) => {
         if (valor === undefined || typeof valor !== 'number') {
             return res.status(400).json({ message: 'Valor inválido ou ausente.' });
         }
+
     }
 
     next();
 };
 
-// Criação das tabelas no banco de dados
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS usuarios (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT UNIQUE,
         password TEXT,
-        dogName TEXT UNIQUE,
-        role TEXT DEFAULT 'user'
+        dogName TEXT UNIQUE
     )`);
 
     db.run(`CREATE TABLE IF NOT EXISTS dados_sensores (
@@ -74,52 +73,7 @@ db.serialize(() => {
     )`);
 });
 
-// Rota para cadastrar um novo usuário
-app.post('/register', async (req, res) => {
-    const { username, password, dogName, role = 'user' } = req.body;
-    try {
-        db.get('SELECT * FROM usuarios WHERE username = ?', [username], async (err, row) => {
-            if (row) {
-                return res.status(400).json({ message: 'Usuário já existe' });
-            }
-
-            const hashedPassword = await bcrypt.hash(password, 10);
-
-            db.run('INSERT INTO usuarios (username, password, dogName, role) VALUES (?, ?, ?, ?)', 
-                [username, hashedPassword, dogName, role], (err) => {
-                if (err) {
-                    console.error('Erro ao cadastrar usuário:', err.message);
-                    return res.status(500).json({ message: 'Erro ao cadastrar usuário' });
-                }
-                res.status(201).json({ message: 'Usuário cadastrado com sucesso' });
-            });
-        });
-    } catch (err) {
-        console.error('Erro ao processar o cadastro:', err.message);
-        res.status(500).json({ message: 'Erro ao processar o cadastro' });
-    }
-});
-
-// Rota para login
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    db.get('SELECT * FROM usuarios WHERE username = ?', [username], async (err, row) => {
-        if (!row) {
-            return res.status(400).json({ message: 'Usuário ou senha incorretos' });
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, row.password);
-        if (!isPasswordValid) {
-            return res.status(400).json({ message: 'Usuário ou senha incorretos' });
-        }
-
-        const token = jwt.sign({ userId: row.id, role: row.role }, SECRET_KEY, { expiresIn: '1h' });
-        res.json({ message: 'Login realizado com sucesso', token });
-    });
-});
-
-// Rota para receber os dados dos sensores
+//rota para inserir dados no banco de dados
 app.post('/dados-sensores', authenticateJWT, validateSensorData, (req, res) => {
     const dadosArray = Array.isArray(req.body) ? req.body : [req.body];
     console.log('Dados recebidos dos sensores:', dadosArray);
@@ -149,6 +103,7 @@ app.post('/dados-sensores', authenticateJWT, validateSensorData, (req, res) => {
         });
 });
 
+
 // Rota para buscar todos os dados dos sensores
 app.get('/dados-sensores', authenticateJWT, (req, res) => {
     const query = `SELECT * FROM dados_sensores`;
@@ -162,6 +117,106 @@ app.get('/dados-sensores', authenticateJWT, (req, res) => {
         }
     });
 });
+
+
+// Rota para cadastrar um novo usuário
+app.post('/register', async (req, res) => {
+    const { username, password, dogName } = req.body;  
+    try {
+        db.get('SELECT * FROM usuarios WHERE username = ?', [username], async (err, row) => {
+            if (row) {
+                return res.status(400).json({ message: 'Usuário já existe' });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            db.run('INSERT INTO usuarios (username, password, dogName) VALUES (?, ?, ?)', 
+                [username, hashedPassword, dogName], (err) => {
+                if (err) {
+                    console.error('Erro ao cadastrar usuário:', err.message);
+                    return res.status(500).json({ message: 'Erro ao cadastrar usuário' });
+                }
+                res.status(201).json({ message: 'Usuário cadastrado com sucesso' });
+            });
+        });
+    } catch (err) {
+        console.error('Erro ao processar o cadastro:', err.message);
+        res.status(500).json({ message: 'Erro ao processar o cadastro' });
+    }
+});
+
+// Rota para recuperação de senha
+app.post('/recover-password', async (req, res) => {
+    const { username, dogName } = req.body;
+
+    db.get('SELECT * FROM usuarios WHERE username = ? AND dogName = ?', [username, dogName], async (err, row) => {
+        if (!row) {
+            return res.status(400).json({ message: 'Usuário ou nome do cachorro incorretos' });
+        }
+
+        res.json({ message: 'Validação bem-sucedida.', password: row.password });
+    });
+});
+
+// Rota para alterar a senha
+app.post('/change-password', async (req, res) => {
+    const { username, newPassword } = req.body;
+
+    db.get('SELECT * FROM usuarios WHERE username = ?', [username], (err, row) => {
+        if (err) {
+            return res.status(500).json({ message: 'Erro ao acessar o banco de dados.' });
+        }
+        if (!row) {
+            return res.status(400).json({ message: 'Usuário não encontrado.' });
+        }
+
+        const hashedPassword = bcrypt.hashSync(newPassword, 10);
+        db.run('UPDATE usuarios SET password = ? WHERE username = ?', [hashedPassword, username], function(err) {
+            if (err) {
+                return res.status(500).json({ message: 'Erro ao atualizar a senha.' });
+            }
+            return res.json({ message: 'Senha alterada com sucesso!' });
+        });
+    });
+});
+
+
+
+// Rota para login
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    db.get('SELECT * FROM usuarios WHERE username = ?', [username], async (err, row) => {
+        if (!row) {
+            return res.status(400).json({ message: 'Usuário ou senha incorretos' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, row.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Usuário ou senha incorretos' });
+        }
+
+        const token = jwt.sign({ userId: row.id }, SECRET_KEY, { expiresIn: '1h' }); 
+        res.json({ message: 'Login realizado com sucesso', token });
+    });
+});
+
+
+// Rota para limpar todos os dados da tabela
+app.delete('/limpar-dados', authenticateJWT, (req, res) => {
+    const query = `DELETE FROM dados_sensores`;
+
+    db.run(query, [], (err) => {
+        if (err) {
+            console.error('Erro ao limpar dados do banco de dados:', err.message);
+            res.status(500).send('Erro ao limpar os dados.');
+        } else {
+            console.log('Dados da tabela limpos com sucesso.');
+            res.send('Dados da tabela foram limpos com sucesso.');
+        }
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
